c8fa513abeec3c1328d5592ec781b76f418397c7
TronkIsHere <137505475+TronkIshere@users.noreply.github.com>
2024-06-11 23:24:35 +0700

    Add mergeSort, quickSort.

--- Practice-DNA/src/main/java/org/example/One_Dimensional_Array.java
+++ Practice-DNA/src/main/java/org/example/One_Dimensional_Array.java
@@ -98,4 +98,91 @@ public class One_Dimensional_Array {
             array[j + 1] = temp;
         }
     }
+
+    //Recursion: đệ quy
+
+    //**************** mergeSort ****************
+    public static void mergeSort(int[] array){
+        int length = array.length;
+        if (length <= 1) return;
+
+        int middle = length/2;
+        int[] leftArray = new int[middle];
+        int[] rightArray = new int[length - middle];
+
+        int i = 0;//left array
+        int j = 0;//right array
+
+        for(; i < length; i++){
+            if(i < middle) {
+                leftArray[i] = array[i];
+            }
+            else{
+                rightArray[j] = array[i];
+                j++;
+            }
+        }
+        mergeSort(leftArray);
+        mergeSort(rightArray);
+        merge(leftArray, rightArray, array);
+    }
+
+    public static void merge(int[] leftArray, int[] rightArray, int[] array){
+
+        int leftSize = array.length/2;
+        int rightSize = array.length/2;
+        int i = 0, l = 0, r = 0; //indices
+
+        //check the conditions for merging
+        while (l < leftSize && r < rightSize) {
+            if(leftArray[l] < rightArray[r]) {
+                array[i] = leftArray[l];
+                i++;
+                l++;
+            }
+            else {
+                array[i] = rightArray[r];
+                i++;
+            }
+        }
+
+        while(l < leftSize) {
+            array[i] = leftArray[l];
+            i++;
+            l++;
+        }
+
+        while(r < rightSize) {
+            array[i] = rightArray[r];
+            i++;
+            r++;
+        }
+    }
+
+    //**************** QuickSort ****************
+    public static void quickSort(int[] array, int start, int end) {
+        if(end <= start) return; //base case
+        int pivot = partition(array, start, end);
+        quickSort(array, start, pivot - 1);
+        quickSort(array, pivot + 1, end);
+    }
+
+    public static int partition(int[] array, int start, int end) {
+        int pivot = array[end];
+        int i = start - 1;
+
+        for (int j = start; j <= end - 1; j++){
+            if (array[i] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        i++;
+        int temp = array[i];
+        array[i] = array[end];
+        array[end] = temp;
+        return i;
+    }
 }
