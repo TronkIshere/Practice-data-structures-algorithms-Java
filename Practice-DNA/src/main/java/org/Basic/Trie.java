d1ade1b9d959029094d319a160de025d08ed058a
TronkIsHere <137505475+TronkIshere@users.noreply.github.com>
2024-06-15 16:21:36 +0700

    Add TrieNode file.

---
+++ Practice-DNA/src/main/java/org/Basic/Trie.java
@@ -0,0 +1,48 @@
+package org.Basic;
+
+public class Trie {
+    private TrieNode root;
+
+    public Trie(){
+        root = new TrieNode(); // root is empty
+    }
+
+    private class TrieNode {
+        private TrieNode[] children;
+        private boolean isWord;
+
+        public TrieNode() {
+            this.children = new TrieNode[26]; //storing english words - a -> z
+            this.isWord = false;
+        }
+    }
+
+    public void insert(String word){
+        TrieNode current = root;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            int index = c - 'a';
+            if(current.children[index] == null){
+                TrieNode node = new TrieNode();
+                current.children[index] = node;
+                current = node;
+            } else {
+                current = current.children[index];
+            }
+        }
+        current.isWord = true;
+    }
+
+    public boolean search(String word){
+        return false;
+    }
+
+    public static void main(String[] args){
+        Trie trie = new Trie();
+        trie.insert("cat");
+        trie.insert("cab");
+        trie.insert("san");
+        trie.insert("so");
+        System.out.println("Value inserted successfully !!!");
+    }
+}
