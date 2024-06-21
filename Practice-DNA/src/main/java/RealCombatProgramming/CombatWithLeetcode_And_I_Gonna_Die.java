e80bbc9281476ca2dd18f0ee3a15ee1227cad94f
TronkIsHere <137505475+TronkIshere@users.noreply.github.com>
2024-06-21 15:33:15 +0700

    Update canJump, isMatch, convertToTitle, titleToNumber, minDepth CombatWithLeetCode.

--- Practice-DNA/src/main/java/RealCombatProgramming/CombatWithLeetcode_And_I_Gonna_Die.java
+++ Practice-DNA/src/main/java/RealCombatProgramming/CombatWithLeetcode_And_I_Gonna_Die.java
@@ -649,6 +649,14 @@ public class CombatWithLeetcode_And_I_Gonna_Die {
         return false;
     }
 
+    /*public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        if (root.left == null && root.right == null) return 1;
+        if (root.left == null) return minDepth(root.right) + 1;
+        if (root.right == null) return minDepth(root.left) + 1;
+        return Math.min(minDepth(root.right), minDepth(root.left)) + 1;
+    }*/
+
     public static void main(String[] args){
         CombatWithLeetcode_And_I_Gonna_Die cb = new CombatWithLeetcode_And_I_Gonna_Die();
 
