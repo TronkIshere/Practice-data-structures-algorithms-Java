e26790d7469346c4d86ba979c2b2703d2238e0de
TronkIsHere <137505475+TronkIshere@users.noreply.github.com>
2024-06-24 02:25:05 +0700

    Update sortList, reverseWordsMedium into CombatWithLeetCode.

--- Practice-DNA/src/main/java/RealCombatProgramming/CombatWithLeetcode_And_I_Gonna_Die.java
+++ Practice-DNA/src/main/java/RealCombatProgramming/CombatWithLeetcode_And_I_Gonna_Die.java
@@ -908,6 +908,57 @@ public class CombatWithLeetcode_And_I_Gonna_Die {
         return result.toString();
     }
 
+    /*public static ListNode merge(ListNode left, ListNode right){
+        ListNode head=new ListNode(0);
+        ListNode temp=head;
+        while(left!=null && right!=null){
+            if(left.val<right.val){
+                temp.next=left;
+                left=left.next;
+            }else{
+                temp.next=right;
+                right=right.next;
+            }
+            temp=temp.next;
+        }
+        if(left!=null){
+            temp.next=left;
+        }
+        if(right!=null){
+            temp.next=right;
+        }
+        return head.next;
+    }
+    public static ListNode findMid(ListNode head){
+        ListNode fast=head;
+        ListNode slow=head;
+        if(head==null||head.next==null){
+            return head;
+        }
+        fast=fast.next;
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode mid=slow.next;
+        slow.next=null;
+        return mid;
+    }
+    public ListNode sortList(ListNode head) {
+        if(head==null||head.next==null){
+            return head;
+        }
+
+
+        ListNode mid=findMid(head);
+        ListNode left=sortList(head);
+        ListNode right=sortList(mid);
+
+
+        ListNode newHead =merge(left,right);
+        return newHead;
+    }*/
+
     public static void main(String[] args){
         CombatWithLeetcode_And_I_Gonna_Die cb = new CombatWithLeetcode_And_I_Gonna_Die();
         cb.moveZeroes(new int[] {1,2});
